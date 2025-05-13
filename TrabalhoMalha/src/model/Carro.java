1c6529436ccbf281569ad16cd485ee6c0f499186
João Henrique <143840523+JoaoHCarvalho@users.noreply.github.com>
2025-05-13 03:05:41 -0300

    Update

--- TrabalhoMalha/src/model/Carro.java
+++ TrabalhoMalha/src/model/Carro.java
@@ -1,4 +1,76 @@
 package model;
 
+import java.util.Random;
+
 public class Carro extends Thread{
+    private int velocidade;
+    private boolean desligado;
+    private Mutex atual;
+
+    public int getVelocidade() {
+        return velocidade;
+    }
+
+    public void setVelocidade(int velocidade) {
+        Random r = new Random();
+        this.velocidade = (int) (1+r.nextFloat()*(10-1)*100)+375;
+    }
+
+    public boolean isDesligado() {
+        return desligado;
+    }
+
+    public void setDesligado(boolean desligado) {
+        this.desligado = desligado;
+    }
+
+    public Mutex getAtual() {
+        return atual;
+    }
+
+    public void setAtual(Mutex atual) {
+        this.atual = atual;
+    }
+
+    public boolean entrarMalha(Mutex entrada){
+        boolean teste = entrada.getSemaphore().tryAcquire();
+        if(teste)
+        {
+            entrada.setCarro(this);
+            this.setAtual(entrada);
+        }
+        return teste;
+    }
+
+    public void moverMalha()
+    {
+        Mutex proximo = (Mutex) atual.getProxima();
+        try
+        {
+            proximo.getSemaphore().acquire();
+            proximo.setCarro(this);
+        } catch (InterruptedException e)
+        {
+            throw new RuntimeException(e);
+        }
+        Integer [][] positions =
+        {
+            {atual.getLinha(),atual.getColuna()},
+            {proximo.getLinha(), proximo.getColuna()}
+        };
+        atual.getSemaphore().release();
+        atual.setCarro(null);
+        atual=proximo;
+    }
+    private void sairMalha(){
+        desligado = true;
+        Integer[][] posicao =
+        {
+            {atual.getLinha(), atual.getColuna()},
+            {null, null}
+        };
+        atual.getSemaphore().release();
+        atual.setCarro(null);
+    }
+
 }
---
+++ TrabalhoMalha/src/model/Estrada.java
@@ -0,0 +1,82 @@
+package model;
+
+public class Estrada {
+    protected boolean saida = false;
+    protected boolean entrada;
+    protected Estrada proxima;
+    protected int direcao;
+    protected Carro carro;
+    protected int linha;
+    protected int coluna;
+
+    public Estrada(int direcao, int linha, int coluna) {
+        this.direcao=direcao;
+        this.linha=linha;
+        this.coluna=coluna;
+        this.proxima=null;
+    }
+
+    public boolean isSaida() {
+        return saida;
+    }
+
+    public void setSaida(boolean saida) {
+        this.saida = saida;
+    }
+
+    public boolean isEntrada() {
+        return entrada;
+    }
+
+    public void setEntrada(boolean entrada) {
+        this.entrada = entrada;
+    }
+
+    public Estrada getProxima() {
+        return proxima;
+    }
+
+    public void setProxima(Estrada[][] proxima) {
+        if(direcao==1&&((linha-1)>=0))
+            this.proxima=proxima[linha-1][coluna];
+        if(direcao==2&&((coluna+1)<proxima[0].length))
+            this.proxima=proxima[linha][coluna+1];
+        if(direcao==3&&((linha+1)<proxima.length))
+            this.proxima=proxima[linha+1][coluna];
+        if(direcao==4&&((coluna-1)>=0))
+            this.proxima=proxima[linha][coluna-1];
+    }
+
+    public int getDirecao() {
+        return direcao;
+    }
+
+    public void setDirecao(int direcao) {
+        this.direcao = direcao;
+    }
+
+    public Carro getCarro() {
+        return carro;
+    }
+
+    public void setCarro(Carro carro) {
+        this.carro = carro;
+    }
+
+    public int getLinha() {
+        return linha;
+    }
+
+    public void setLinha(int linha) {
+        this.linha = linha;
+    }
+
+    public int getColuna() {
+        return coluna;
+    }
+
+    public void setColuna(int coluna) {
+        this.coluna = coluna;
+    }
+
+}
